5bc0a70aba81ec25e4d973c5ac385f3a3f46f531
aimansahar330-oss <aimansahar330@gmail.com>
2026-06-20 14:40:39 +0500

    third commit

--- src/services/taskApi.js
+++ src/services/taskApi.js
@@ -1,4 +1,6 @@
-const API_URL = "http://localhost:5000/api/tasks";
+const API_URL = import.meta.env.VITE_API_URL 
+  ? `${import.meta.env.VITE_API_URL}/tasks` 
+  : "/api/tasks";
 
 //  token helper
 const getToken = () => localStorage.getItem("token");
